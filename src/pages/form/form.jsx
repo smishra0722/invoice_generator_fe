c25d36a72d98be5e740628b909771d83355e2ae8
mayurssharma <mayurssharma99@gmail.com>
2022-06-22 23:07:54 +0530

    Add Input

---
+++ src/pages/form/form.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './form.css'
+
+const Form = () => {
+    return <div className='Form'>
+        <p>Name:<input type="text"placeholder='Full name' /></p>
+    </div>
+}
+
+export default Form
