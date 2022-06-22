819c7d72a5c9b2273730de59775ca59164d3d39d
mayurssharma <mayurssharma99@gmail.com>
2022-06-22 23:13:00 +0530

    Some Changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import Home from './pages/home/home'
+import Form from './pages/form/form'
+import {Routes, Route, Link} from 'react-router-dom'
 
 function App() {
+  const [isFormPage, setIsFormPage] = React.useState(false)
   return (
     <div className="App">
-      Invoice generator
+        <Link to={isFormPage ? '/form' : '/'} onClick={() => setIsFormPage((prev) => !prev)}>Go to {isFormPage ? 'Form' : 'Home'} Page</Link>
+    <Routes>
+    <Route exact path="/" element={<Home />} />
+    <Route exact path="/form" element={<Form />} />
+  </Routes>
     </div>
   );
 }
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+    return <div className='Home'>
+        Homepage
+    </div>
+}
+
+export default Home
