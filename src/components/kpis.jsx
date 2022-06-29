3fda7b197c7adad50cc0fe567b2fec69b732e965
Shivam Mishra <shivam.mishra@putler.com>
2022-06-29 08:31:35 +0530

    New changes

--- src/components/kpis.jsx
+++ src/components/kpis.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from "react";
+import { FormattedNumber } from "react-intl";
 
 const KPIs = () => {
-    return <div className={'home-kpis'}>
-        {[{title: 'Net Transactions', value: '$30,000'},{title: 'Monthly', value: '$30,000'},{title: 'Weekly', value: '$30,000'}].map((value) => {
-            return <div className={'kpi'}>
-                <h1>{value.title}</h1>
-                <div>{value.value}</div>
+  return (
+    <div className={"home-kpis"}>
+      {[
+        { title: "Net Transactions", value: 30000 },
+        { title: "Monthly", value: 2400 },
+        { title: "Weekly", value: 1500 },
+      ].map((value) => {
+        return (
+          <div className={"kpi"}>
+            <h1>{value.title}</h1>
+            <div>
+              <FormattedNumber
+                style={"currency"}
+                currency={"INR"}
+                value={value.value}
+              />
             </div>
-        })}
+          </div>
+        );
+      })}
     </div>
-}
+  );
+};
 
-export default KPIs
+export default KPIs;
